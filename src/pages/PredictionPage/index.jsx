33228f8d60a01ab1729759b35517e64d44ff446a
srikanth <srikanth@shoora.com>
2023-09-19 15:26:33 +0530

    payment changes

--- src/pages/PredictionPage/index.jsx
+++ src/pages/PredictionPage/index.jsx
@@ -1,40 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import { useDispatch } from 'react-redux';
 import PredictionCard from '../../component/PredectionCard';
 import './Prediction.css';
 import { Header } from '../../component';
 import { getLocalStorage } from '../../utility/storageutility';
 import { accessTokenKey } from '../../constants/storageconstants';
-import { getpredictionpage } from '../../store/predict/predictthunk';
-import { generateApiUrl } from '../../api/apihelper';
-
-const matchesData = [
-  {
-    id: 1,
-    title: 'INDIA vs PAKISTAN',
-    date: '2023-09-01',
-    // Add other match details
-  },
-  // Add more match objects
-];
 
 const PredictionPage = () => {
-  const [predictionData, setPredictionData] = useState([]); // State to store prediction data
+  const [predictionData, setPredictionData] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
     getPredictionApiCall();
+    loadRazorpay(); // Load Razorpay script when the component mounts
   }, []);
 
-  async function getPredictionApiCall() {
-    try {
-      const apiUrl = 'http://localhost:8000/prediction/'; // Replace with your API endpoint
+  const loadRazorpay = () => {
+    const script = document.createElement('script');
+    script.src = 'https://checkout.razorpay.com/v1/checkout.js';
+    script.async = true;
+    script.onload = () => {
+      console.log('Razorpay script loaded');
+    };
+    document.body.appendChild(script);
+  };
 
-      // Retrieve the authorization token from local storage
-      const authToken = getLocalStorage(accessTokenKey); // Fetch the token from local storage
+  const getPredictionApiCall = async () => {
+    try {
+      const apiUrl = 'http://localhost:8000/prediction/';
+      const authToken = getLocalStorage(accessTokenKey);
 
       const headers = {
-        Authorization: `JWT ${authToken}`, // Include the token in the headers
+        Authorization: `JWT ${authToken}`,
       };
 
       const response = await fetch(apiUrl, { headers });
@@ -42,23 +40,104 @@ const PredictionPage = () => {
       if (response.ok) {
         const data = await response.json();
         console.log('API Response:', data);
-
-        // Update the state with the fetched data
         setPredictionData(data.predicted_teams);
       } else {
         console.error('Error fetching prediction data:', response.status, response.statusText);
-        // Handle the error gracefully, e.g., set an error state
       }
     } catch (error) {
       console.error('Error fetching prediction data:', error);
-      // Handle the error gracefully, e.g., set an error state
     }
-  }
+  };
 
-  const handlePay = (matchId) => {
-    // Implement payment logic for the specific match
-    window.location.href = "https://checkout.razorpay.com/v1/pay/order_MdtNwoLs6YH3VK";
-    console.log('Paid amount for match with ID:', matchId);
+  const handlePay = (uuid, price) => {
+    showRazorpay(uuid, price);
+    console.log('Paid amount for match with UUID:', uuid);
+  };
+
+  const showRazorpay = async (uuid, price) => {
+    try {
+      const authToken = getLocalStorage(accessTokenKey);
+      const bodyData = {
+        // Include the data you want to send in the request body
+      };
+
+      const headers = {
+        Authorization: `JWT ${authToken}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      };
+
+      const response = await Axios.post(
+        `http://localhost:8000/prediction/${uuid}/`,
+        bodyData,
+        { headers }
+      );
+
+      if (response.status === 200) {
+        console.log('Prediction POST request successful:', response.data);
+        // Handle success as needed
+
+        // After successfully creating the prediction, open Razorpay payment UI
+        const options = {
+          key_id: 'rzp_test_pUAFN3wugOV9Ek',
+          key_secret: 'zilYJHW6eX7JaESyFkJfR0UY',
+          amount: 200 * 100, // Amount in paise (Razorpay expects the amount in paise)
+          currency: 'INR',
+          name: 'Org. Name',
+          description: 'Test transaction',
+          image: '', // Add image URL
+          order_id: 'order_Me8pugBP4fn4ir',
+          handler: function (response) {
+            handlePaymentSuccess(response);
+          },
+          prefill: {
+            name: "User's name",
+            email: "User's email",
+            contact: "User's phone",
+          },
+          notes: {
+            address: "Razorpay Corporate Office",
+          },
+          theme: {
+            color: "#3399cc",
+          },
+        };
+
+        var rzp1 = new window.Razorpay(options);
+        rzp1.open();
+      } else {
+        console.error('Prediction POST request failed with status code:', response.status);
+        // Handle other status codes as needed
+      }
+    } catch (error) {
+      console.error('Error making prediction POST request:', error);
+      // Handle other errors as needed
+    }
+  };
+
+  const handlePaymentSuccess = async (response) => {
+    try {
+      const bodyData = new FormData();
+      bodyData.append('response', JSON.stringify(response));
+
+      await Axios({
+        url: `http://localhost:8000/success/`,
+        method: 'POST',
+        data: bodyData,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((res) => {
+          console.log('Everything is OK!');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.error('Error making payment success POST request:', error);
+    }
   };
 
   return (
@@ -66,12 +145,12 @@ const PredictionPage = () => {
       <Header title="Match Predictions" />
       <div className='page-content'>
         <div className="prediction-page">
-        {predictionData.map((prediction) => (
+          {predictionData.map((prediction) => (
             <PredictionCard
               key={prediction.uuid}
               teamName={prediction.teamName}
               price={prediction.price}
-              onPay={() => handlePay(prediction.uuid)}
+              onPay={() => handlePay(prediction.uuid, prediction.price)}
             />
           ))}
         </div>
