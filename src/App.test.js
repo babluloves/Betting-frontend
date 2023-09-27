b57c01ccc70fcb3cc789feb43b45731e16c25e6a
babluloves <144165410+babluloves@users.noreply.github.com>
2023-09-27 15:11:27 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter
+
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  // Your test assertions here
 });
